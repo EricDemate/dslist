346784dd1e9dc5e3a94dceeb3cbbccce289f367a
Eric L Demate <eric.demate@gmail.com>
2024-11-28 18:22:13 -0300

    adicionado o modelo de dominio

--- src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -2,9 +2,19 @@ package com.devsuperior.dslist.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
 public class BelongingPK {
-	
+
+	@ManyToOne
+	@JoinColumn(name = "game_id")
 	private Game game;
+
+	@ManyToOne
+	@JoinColumn(name = "list_id")
 	private GameList list;
 
 	public BelongingPK() {
@@ -49,7 +59,5 @@ public class BelongingPK {
 		BelongingPK other = (BelongingPK) obj;
 		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
 	}
-	
-	
 
 }
